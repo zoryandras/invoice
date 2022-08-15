7a62b584d2106126daca306d68019cd07e6909f4
Andras Varga <zoryandras@gmail.com>
2022-08-16 00:46:36 +0200

    Added github pages

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders page', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/invoices/);
-  expect(linkElement).toBeInTheDocument();
-});
