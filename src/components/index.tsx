d7df20820273d5749c14a169df1d9d56a58e3900
Andras Varga <zoryandras@gmail.com>
2022-08-15 21:29:04 +0200

    Added home index.ts

---
+++ src/components/index.tsx
@@ -0,0 +1,39 @@
+import InvoicesHeader from './home/InvoicesHeader';
+import Invoices from './home/Invoices';
+import Layout from './shared/Layout';
+import Navbar from './shared/Navbar';
+import Status from './invoice/Status';
+import InvoiceInfo from './invoice/InvoiceInfo';
+import InvoiceModal from './shared/InvoiceModal';
+import InputElement from './form/InputElement';
+import SelectElement from './form/SelectElement';
+import ItemList from './form/ItemList';
+import CreateInvoiceForm from './form/CreateInvoiceForm';
+import EditInvoiceForm from './form/EditInvoiceForm';
+import Loading from './shared/Loading';
+import DateElement from './form/DateElement';
+import Seo from './shared/Seo';
+import EmptyInvoices from './home/EmptyInvoices';
+import EmptyInvoice from './invoice/EmptyInvoice';
+import MyModal from './shared/MyModal';
+
+export {
+   Invoices,
+   Status,
+   InvoicesHeader,
+   Layout,
+   Navbar,
+   InvoiceInfo,
+   InvoiceModal,
+   CreateInvoiceForm,
+   EditInvoiceForm,
+   InputElement,
+   SelectElement,
+   ItemList,
+   Loading,
+   DateElement,
+   Seo,
+   EmptyInvoices,
+   EmptyInvoice,
+   MyModal,
+};
